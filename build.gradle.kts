f02ee2a1d1a0830c837095c890e899cb1490a7e4
Daksh Semwal <dakshsemwal35@gmail.com>
2023-04-21 14:52:25 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+plugins {
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    kotlin("android") apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.versions)
+    cleanup
+    base
+}
+
+allprojects {
+    group = PUBLISHING_GROUP
+}
+
+val detektFormatting = libs.detekt.formatting
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    detekt {
+        config = rootProject.files("config/detekt/detekt.yml")
+    }
+
+    dependencies {
+        detektPlugins(detektFormatting)
+    }
+}
+
+tasks {
+    withType<DependencyUpdatesTask>().configureEach {
+        rejectVersionIf {
+            candidate.version.isStableVersion().not()
+        }
+    }
+}
